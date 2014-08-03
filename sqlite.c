b8fd46d12860d3648be73b5e94ec8da4662091bd
DE8MSH <de8msh@darc.de>
2014-08-03 17:11:00 +0200

    doest'n work

--- sqlite.c
+++ sqlite.c
@@ -44,6 +44,7 @@ if (mm->msgtype == 0) {
 
 /* DF 4/20 (Surveillance (roll call) Altitude has: altitude, icao, flight status, DR, UM) */
 /* TODO flight status, DR, UM */
+/*
 if (mm->msgtype == 4 || mm->msgtype == 20){
 
 sql = sqlite3_mprintf( "INSERT INTO flights (modes, alt, df, msgs) VALUES ('%06X', '%d', '%d', '%ld')",mm->addr, a->altitude, mm->msgtype, a->messages);
@@ -55,10 +56,11 @@ sql = sqlite3_mprintf( "INSERT INTO flights (modes, alt, df, msgs) VALUES ('%06X
       fprintf(stdout, "msgtype 4/20 == OK\n");
    }
 }
-
+*/
 
     /* DF 5/21 (Surveillance (roll call) IDENT Reply, has: alt, icao, flight status, DR, UM, squawk) */
-    if (mm->msgtype == 5 || mm->msgtype == 21) {
+/*  
+  if (mm->msgtype == 5 || mm->msgtype == 21) {
 sql = sqlite3_mprintf( "INSERT INTO flights (modes, alt, squawk, df, msgs) VALUES ('%06X', '%d', '%d', '%d', '%ld')",mm->addr, mm->altitude, mm->modeA, mm->msgtype, a->messages);
   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
   if( rc != SQLITE_OK ){
@@ -68,8 +70,9 @@ sql = sqlite3_mprintf( "INSERT INTO flights (modes, alt, squawk, df, msgs) VALUE
       fprintf(stdout, "msgtype 5/21 == OK\n");
    }
 }
-
+*/
  /* DF 11 */
+/*
 if (mm->msgtype == 11) {
 
 sql = sqlite3_mprintf( "INSERT INTO flights (modes, df, msgs) VALUES ('%06X', '%d', '%ld')",mm->addr, mm->msgtype, a->messages);
@@ -81,9 +84,10 @@ sql = sqlite3_mprintf( "INSERT INTO flights (modes, df, msgs) VALUES ('%06X', '%
       fprintf(stdout, "msgtype 11 == OK\n");
    }
 }
-
+*/
     /* DF17 *with or without position data */
-    if (mm->msgtype == 17) {
+/*  
+  if (mm->msgtype == 17) {
 sql = sqlite3_mprintf( "INSERT INTO flights (df, flight, airline, modes, alt, vr, lat, lon, speed, heading, msgs) VALUES ('%d', '%s', '%3s', '%06X', '%d', '%d', '%1.5f', '%1.5f', '%d', '%d', '%ld')",mm->msgtype, a->flight, a->flight, mm->addr, mm->altitude, mm->vert_rate, a->lat, a->lon, a->speed, a->track, a->messages);
   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
   if( rc != SQLITE_OK ){
@@ -93,6 +97,7 @@ sql = sqlite3_mprintf( "INSERT INTO flights (df, flight, airline, modes, alt, vr
       fprintf(stdout, "msgtype 17 == OK\n");
    }
 }
+*/
 /////////////////////////
 }
 sqlite3_close(db);
